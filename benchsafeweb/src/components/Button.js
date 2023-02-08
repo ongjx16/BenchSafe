39eb4aff1ffee29f107acb3618b47bdbb04556cc
ongjx16 <ongjx16@gmail.com>
2023-02-08 16:54:34 +0800

    added responsive css

--- benchsafeweb/src/components/Button.js
+++ benchsafeweb/src/components/Button.js
@@ -1,14 +1,13 @@
 
-export default function Button ({ text = "Button", onClickHandler, className }) {
-  return (
-    <button
-      className={`${className} text-black flex items-center rounded-full hover:scale-105 transition-all duration-150 ease-linear bg-purple-200 drop-shadow-lg `}
-      onClick={onClickHandler}
-    >
-        <p className="font-poppins font-bold py-5 ">
-        {text}
-        </p>
-    </button>
-  );
+export default function Button({ text = "Button", onClickHandler, className }) {
+    return (
+        <button
+            className={`${className} text-black flex w-5/6 items-center rounded-full hover:scale-105 transition-all duration-150 ease-linear bg-gradient-to-r from-blue-300 to-blue-600 drop-shadow-lg `}
+            onClick={onClickHandler}>
+            <p className="font-poppins font-bold py-5">
+                {text} hello
+            </p>
+        </button>
+    );
 };
 

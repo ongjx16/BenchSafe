25406cea1ac44737caa00ba073457d3401035e5c
hohanyu <79267462+hohanyu@users.noreply.github.com>
2023-03-20 22:06:00 +0800

    Created new pages

---
+++ benchsafeweb/src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from 'next/link';
+
+export default function LandingPage() {
+    return(
+        <div>
+            <h1>LandingPage</h1>
+        </div>
+    )
+};

08c434f22245592bf6a680ebac758a2b1086c630
hohanyu <79267462+hohanyu@users.noreply.github.com>
2023-03-22 03:24:26 +0800

    Finished Landing Page

--- benchsafeweb/src/pages/LandingPage.js
+++ benchsafeweb/src/pages/LandingPage.js
@@ -1,49 +1,94 @@
 import React from "react";
 import Link from 'next/link';
-import Image from "next/image";
 import Home from "../assets/Home.svg";
+import Image from "next/image";
 import Location from "../assets/Location.svg";
 import AddBench from "../assets/AddBench.svg";
 import Profile from "../assets/Profile.svg";
+import FlatBenchPress from "../images/FlatBenchPress.jpg";
+import InclineBenchPress from "../images/InclineBenchPress.jpg";
 
 function LandingPage() {
     return(
-        <div className="flex flex-col h-screen justify-between ">
-            <header className="grow-0 h-14 flex flex-row justify-center">
-                <div>
-                    <Image src={Location} alt="Location" className="m-1"width={18} height={30} />
+        <div className="flex flex-col h-screen justify-between w-screen">
+
+            {/* Header design */}
+            <header >
+                <div className="grow-0 h-14 flex flex-row justify-center">
+                    <div>
+                        <Image src={Location} alt="Location" className="m-1"width={18} height={30} />
+                    </div>
+                    <div>
+                        <h1 className="flex justify-center">EverydayFitness Jurong</h1>
+                    </div>
                 </div>
-                <div>
-                    <h1 className="flex justify-center">EverydayFitness Jurong</h1>
-                </div>      
+
+                <div className="">
+                    <h2 className="flex justify-center m-5">Please Select A Bench</h2>    
+                </div>     
             </header>
 
-            <div className="grow w-screen h-full overflow-y-auto">
-                <div>
-                    <h2 className="flex justify-center m-5">Select Bench to Use</h2>    
-                </div>
+            {/* Body design */}
+            <div className="grow w-full h-full overflow-y-auto">
 
                 <div className="flex justify-center h-full">
                     <div>
                         {/* Flat Bench */}
-                        <div>
-                            <div className="w-screen h-64 rounded-xl bg-white-300 shadow-xl m-2">
-                                <p className="flex flex-row justify-center">Flat bench</p>
+                        <div class="flex justify-center">
+                            <div
+                                class="block max-w-sm rounded-lg bg-white shadow-lg">
+                                <a href="#!" data-te-ripple-init data-te-ripple-color="light">
+                                <Image 
+                                    src={FlatBenchPress} 
+                                    alt="FlatBenchPress"
+                                    class="rounded-t-lg"/>
+                                </a>
+                                <div class="p-6">
+                                <h5
+                                    class="mb-2 text-xl font-medium leading-tight text-neutral-800">
+                                    Bench 1
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600">
+                                    Rogue’s fully updated Westside Bench is a heavy-duty juggernaut inspired by Westside Barbell’s original designs.
+                                </p>
+                                <Link href="/EnterHeight">
+                                    <button className="button">Select</button>
+                                </Link>
+                                </div>
                             </div>
                         </div>
+                            {/* <Image src={FlatBenchPress} alt="FlatBenchPress" className="h-auto max-w-full h-64 rounded-xl bg-white-300 shadow-xl m-2"/> */}
 
                         {/* Incline Bench */}
-                        <div>
-                            <div className="w-screen h-64 rounded-xl bg-white-300 shadow-xl">
-                                <p className="flex flex-row justify-center">Inclined bench</p>
+                        <div class="flex justify-center">
+                            <div
+                                class="block max-w-sm rounded-lg bg-white shadow-lg">
+                                <a href="#!" data-te-ripple-init data-te-ripple-color="light">
+                                <Image 
+                                    src={InclineBenchPress} 
+                                    alt="InclineBenchPress"
+                                    class="rounded-t-lg"/>
+                                </a>
+                                <div class="p-6">
+                                <h5
+                                    class="mb-2 text-xl font-medium leading-tight text-neutral-800">
+                                    Bench 2
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600">
+                                    Made in the USA from 3x3" 11-Gauge Steel, the Monster Westside Bench stands among the most versatile, heavy-duty, uncompromising bench racks ever built
+                                </p>
+                                <Link href="/EnterHeight">
+                                    <button className="button">Select</button>
+                                </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
+            {/* Bottom navigation bar design*/}
             <footer className="flex flex-row justify-center w-screen h-24">
-                {/* Bottom navigation bar design*/}
                     <div className="grid grid-cols-3 bg-slate-100 rounded min-h-fit min-w-fit w-screen drop-shadow-md inline-block">
                         {/* Home */}
                         <Link href="/LandingPage" className="flex justify-center">

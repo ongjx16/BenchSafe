f76b36283dd59f3975636d0ad4e6bdaa98e59bb9
ongjx16 <ongjx16@gmail.com>
2023-03-23 00:53:41 +0800

    flow from tensorflow to exercise type done

--- benchsafeweb/src/pages/SelectExercise.js
+++ benchsafeweb/src/pages/SelectExercise.js
@@ -1,48 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from 'next/link';
 import Image from "next/image";
 import BackButton from "../assets/BackButton.svg";
 import Detail from "../assets/Detail.svg";
 import FlatBenchPress from "../assets/FlatBenchPress.svg";
+import FlatDeactivated from "../assets/FlatDeactivated.svg";
 import InclineBenchPress from "../assets/InclineBenchPress.svg";
+import InclinedActivated from "../assets/InclinedActivated.svg";
+import { useRouter } from 'next/router'
+import Router from "next/router";
 
 function SelectExercise() {
-    return(
-        <div className="flex flex-row justify-center">
-        <div className="auth-form-container">
-            <div>
 
-            {/* Header */}
-            <div className="grid grid-cols-4">
-                <div className="flex justify-self-start col-span-1">
-                    <Link href="/LandingPage">
-                        <Image src={BackButton} alt="BackButton" className="m-1"width={40} height={40} />
-                    </Link>
+    const router = useRouter();
+    console.log(router.query.nipHipH)
+    const [exercise, setExercise] = useState('')
+    return (
+        <div className="flex flex-row justify-center w-screen">
+            <div className="auth-form-container">
+                <div>
+
+                    {/* Header */}
+                    <div className="grid grid-cols-4">
+                        <div className="flex justify-self-start col-span-1">
+                            <Link href="/LandingPage">
+                                <Image src={BackButton} alt="BackButton" className="m-1" width={40} height={40} />
+                            </Link>
+                        </div>
+
+                        <h1 className="flex justify-center col-span-2 py-2">Bench 1</h1>
+
+                        <div className="flex justify-self-end col-span-1">
+                            <Image src={Detail} alt="Detail" className="m-1" width={40} height={40} />
+                        </div>
+                    </div>
+
+                    {/* Bench information */}
+                    <div className="my-5">
+                        <h2>Select Your Exercise</h2>
+                    </div>
+                    <div className="flex flex-row items-center justify-center mx-10">
+                        <div>
+                        {exercise == "flat" ? (
+                        <Image src={FlatBenchPress} alt="FlatBenchPress"
+                            className="h-38"
+                        />)
+                            :
+                            (
+                                <Image src={FlatDeactivated} alt="FlatBenchPress"
+                                    className="h-38"
+                                    onClick={(e) => {
+                                        setExercise("flat");
+                                    }} />
+                            )}
+                        </div>
+                        <div>
+                        {exercise == "inclined" ? (
+                            <Image src={InclinedActivated} alt="InclineBenchPress" className="h-60"
+                            />
+                        ) : (
+                            <Image src={InclineBenchPress} alt="InclineBenchPress" className=" h-60"
+                                onClick={(e) => {
+                                    setExercise("inclined");
+                                }}
+                            />
+                        )}
+                        </div>
+
+                        
+
+                    </div>
                 </div>
-                
-                <h1 className="flex justify-center col-span-2 py-2">Bench 1</h1>
 
-                <div className="flex justify-self-end col-span-1">
-                    <Image src={Detail} alt="Detail" className="m-1"width={40} height={40} />
-                </div>
-            </div>
-            
-            {/* Bench information */}
-            <div className="my-5">
-                <h2>Select Your Exercise</h2>
-            </div>
-                <div className="flex flex-row">
-                    <Image src={FlatBenchPress} alt="FlatBenchPress" className=""/>
-                    <Image src={InclineBenchPress} alt="InclineBenchPress" className="m-1"/>
-                </div>
-            </div>
+                {/* Add button */}
+                <button className="button"
+                    onClick={(e) => {
+                        //submit values
+                        Router.push({
+                            pathname: '/LaserMarking',
+                            query: { nipHipH: router.query.nipHipH, exercise: exercise },
+                        })
 
-            {/* Add button */}
-            <Link href="/LaserMarking">
-                <button className="button">Next</button>
-            </Link>
+                    }}
+                >Next</button>
 
-        </div>
+            </div>
         </div>
     )
 };

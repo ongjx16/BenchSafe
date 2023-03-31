bc7e73b4405c6f38b44e33546a1350510d634bed
hohanyu <79267462+hohanyu@users.noreply.github.com>
2023-04-01 00:03:52 +0800

    Finished directory page

--- benchsafeweb/src/pages/AddNewBench.js
+++ benchsafeweb/src/pages/AddNewBench.js
@@ -22,7 +22,7 @@ return(
           {/* Header */}
           <div className="grid grid-cols-4">
             <div className="flex justify-self-start col-span-1">
-                <Link href="/LandingPage">
+                <Link href="/AdminLandingPage">
                     <Image src={BackButton} alt="BackButton" className="m-1"width={40} height={40} />
                 </Link>
             </div>
--- benchsafeweb/src/pages/Directory.js
+++ benchsafeweb/src/pages/Directory.js
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
 import Link from 'next/link';
 import Image from "next/image";
+import Admin from "../assets/Admin.svg";
+import User from "../assets/User.svg";
 
 
 function Directory(){
 
     return(
         <div className="flex flex-row justify-center w-screen">
-            <div className="auth-form-container w-full">
+            <div className="flex flex-col justify-center text-center w-full">
                     {/* Header */}
-                    <div>
+                    <div className="my-2">
                         <h1 className="my-3">Welcome Back</h1>
                         <h2>How You Would Like To Login?</h2>
                     </div>
                     
                     {/* Login options */}
-                    <div className="grid grid-rows-2">
+                    <div className="grid grid-rows-2 h-screen">
 
                         {/* admin */}
-                        <div>
-                            
+                        <div className="row-start-1 row-span-1">
+                              <div class="overflow-hidden rounded-xl bg-white shadow-2xl">
+                                <Image src={Admin} alt="Admin" class="h-auto w-full" />
+                                <div class="p-5">
+                                <h1 class="text-large mb-5">Admin</h1>
+                                <Link href="/AdminLandingPage">
+                                    <button class="w-full rounded-md py-2 text-white">Login</button>
+                                </Link>
+                                </div>
+                            </div>
                         </div>
 
                         {/* user */}
-                        <div>
-
+                        <div className="row-start-2 row-span-1">
+                              <div class="overflow-hidden rounded-xl bg-white shadow-2xl">
+                                <Image src={User} alt="User" class="h- w-full" />
+                                <div class="p-5">
+                                <h1 class="text-large mb-5">User</h1>
+                                <Link href="/UserLandingPage">
+                                    <button class="w-full rounded-md py-2 text-white">Login</button>
+                                </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
             </div>
@@ -33,3 +51,5 @@ function Directory(){
 }
 
 export default Directory;
+
+{/* <Image src={User} alt="User"/> */}
